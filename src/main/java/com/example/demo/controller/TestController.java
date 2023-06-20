f9f586fa7a68a22066e7dd50e50f9fd0b333dd57
Lee Yu Jeong <uujeong59@sookmyung.ac.kr>
2023-06-20 23:36:19 +0900

    백엔드 개발 16. @Repository 사용하기

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.service.TodoService;
 
